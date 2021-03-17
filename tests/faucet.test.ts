b862563e58e6555bda115e5fefb62f8a82b5dc53
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-03-17 12:42:56 +0100

    Improve faucet tests

--- tests/faucet.test.ts
+++ tests/faucet.test.ts
@@ -1,28 +1,25 @@
-import { LumClient, LumConstants, LumMessages, LumWallet } from '../src';
+import { LumClient, LumConstants, LumMessages, LumUtils, LumWallet } from '../src';
 
-describe('LumClient', () => {
+describe('Faucet', () => {
     let clt: LumClient;
     let w1: LumWallet;
 
     beforeAll(async () => {
         clt = await LumClient.connect('http://node0.testnet.lum.network/rpc');
-        w1 = await LumWallet.fromMnemonic('noodle hope lounge dismiss erase elephant seek crawl check equal city chest');
-        console.log(w1.address);
+        w1 = await LumWallet.fromMnemonic(LumUtils.generateMnemonic());
     });
 
     afterAll(async () => {
         await expect(clt.disconnect()).resolves.toBeTruthy();
     });
 
-    it('should build and dispatch a mint message', async () => {
+    it('Should build and dispatch a mint message', async () => {
         const mintMsg = LumMessages.BuildMsgMintAndSend(w1.address, new Date());
-
         const fee = {
             amount: [{ denom: LumConstants.LumDenom, amount: '0' }],
             gas: '100000',
         };
-
-        const broadcastResult = await clt.signAndBroadcastTx(w1, [mintMsg], fee, 'You mad bro? You aint got money bro?');
-        console.log(broadcastResult);
+        const broadcastResult = await clt.signAndBroadcastTx(w1, [mintMsg], fee, 'I need a dollar!', false);
+        expect(LumUtils.broadcastTxCommitSuccess(broadcastResult)).toBeTruthy();
     });
 });
