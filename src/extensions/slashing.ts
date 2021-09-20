8024d3801a5f7f7334cf4f6fdc1694c198987bae
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-09-20 10:02:26 +0200

    Clean up wip fn

--- src/extensions/slashing.ts
+++ src/extensions/slashing.ts
@@ -3,16 +3,12 @@ import { QueryClient } from '@cosmjs/stargate';
 import { QueryClientImpl, QueryParamsResponse, QuerySigningInfoResponse, QuerySigningInfosResponse } from '../codec/cosmos/slashing/v1beta1/query';
 import { PageRequest } from '../codec/cosmos/base/query/v1beta1/pagination';
 import { createProtobufRpcClient } from './utils';
-import { Bech32 } from '@cosmjs/encoding';
-import { Uint64 } from '@cosmjs/math';
-import { ValidatorSigningInfo } from '../codec/cosmos/slashing/v1beta1/slashing';
 
 export interface SlashingExtension {
     readonly slashing: {
         readonly params: () => Promise<QueryParamsResponse>;
         readonly signing_info: (consAddress: string) => Promise<QuerySigningInfoResponse>;
         readonly signing_infos: (pagination?: PageRequest) => Promise<QuerySigningInfosResponse>;
-        readonly missed_blocks: (consAddress: string, index: number) => Promise<void>;
     };
 }
 
@@ -37,17 +33,6 @@ export function setupSlashingExtension(base: QueryClient): SlashingExtension {
                     pagination,
                 });
                 return response;
-            },
-            missed_blocks: async (consAddress: string, index: number): Promise<void> => {
-                // https://github.com/cosmos/cosmos-sdk/blob/be4a965599dfae6992451e4b5135c487ed45053b/x/slashing/types/keys.go#L39
-                const b = Uint64.fromNumber(index).toBytesLittleEndian();
-                const addr = Bech32.decode(consAddress).data;
-                const key = Uint8Array.from([...Uint8Array.from([2]), ...Uint8Array.from([addr.length]), ...addr, ...b]);
-                // const key = Uint8Array.from([...Uint8Array.from([1]), ...Uint8Array.from([addr.length]), ...addr]);
-                // console.log(key);
-                const responseData = await base.queryVerified('slashing', key);
-                console.log(responseData);
-                // console.log(ValidatorSigningInfo.decode(responseData));
             },
         },
     };
