6db8450f6e40f1cfca3c6bd725ea3364340de447
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-10-01 16:27:21 +0200

    Update registry with new types

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -3,12 +3,20 @@ import { AminoTypes } from '@cosmjs/stargate';
 
 import { Tx } from '../codec/cosmos/tx/v1beta1/tx';
 import { PubKey } from '../codec/cosmos/crypto/secp256k1/keys';
-import { BaseAccount } from '../codec/cosmos/auth/v1beta1/auth';
+import { BaseAccount, ModuleAccount, Params as AuthParams } from '../codec/cosmos/auth/v1beta1/auth';
+import { MsgExec, MsgGrant, MsgRevoke } from '../codec/cosmos/authz/v1beta1/tx';
 import { MsgSend, MsgMultiSend } from '../codec/cosmos/bank/v1beta1/tx';
+import { Coin, DecCoin, DecProto, IntProto } from '../codec/cosmos/base/v1beta1/coin';
+import { CommunityPoolSpendProposal, CommunityPoolSpendProposalWithDeposit } from '../codec/cosmos/distribution/v1beta1/distribution';
 import { MsgFundCommunityPool, MsgSetWithdrawAddress, MsgWithdrawDelegatorReward, MsgWithdrawValidatorCommission } from '../codec/cosmos/distribution/v1beta1/tx';
-import { MsgBeginRedelegate, MsgCreateValidator, MsgDelegate, MsgEditValidator, MsgUndelegate } from '../codec/cosmos/staking/v1beta1/tx';
+import { MsgGrantAllowance, MsgRevokeAllowance } from '../codec/cosmos/feegrant/v1beta1/tx';
+import { Proposal, TextProposal } from '../codec/cosmos/gov/v1beta1/gov';
 import { MsgDeposit, MsgSubmitProposal, MsgVote } from '../codec/cosmos/gov/v1beta1/tx';
-import { MsgClaimBeam, MsgOpenBeam, MsgUpdateBeam } from '../codec/beam/tx';
+import { ParameterChangeProposal } from '../codec/cosmos/params/v1beta1/params';
+import { MsgUnjail } from '../codec/cosmos/slashing/v1beta1/tx';
+import { MsgBeginRedelegate, MsgCreateValidator, MsgDelegate, MsgEditValidator, MsgUndelegate } from '../codec/cosmos/staking/v1beta1/tx';
+import { CancelSoftwareUpgradeProposal, SoftwareUpgradeProposal } from '../codec/cosmos/upgrade/v1beta1/upgrade';
+
 import {
     MsgAcknowledgement,
     MsgChannelCloseConfirm,
@@ -25,23 +33,45 @@ import { MsgCreateClient, MsgSubmitMisbehaviour, MsgUpdateClient, MsgUpgradeClie
 import { MsgConnectionOpenAck, MsgConnectionOpenConfirm, MsgConnectionOpenInit, MsgConnectionOpenTry } from '../codec/ibc/core/connection/v1/tx';
 import { MsgTransfer } from '../codec/ibc/applications/transfer/v1/tx';
 
+import { MsgClaimBeam, MsgOpenBeam, MsgUpdateBeam } from '../codec/beam/tx';
+
 const registryTypes: Iterable<[string, GeneratedType]> = [
-    ['/cosmos.crypto.ed25519.PubKey', PubKey as GeneratedType],
-    ['/cosmos.auth.v1beta1.BaseAccount', BaseAccount as GeneratedType],
-    ['/cosmos.bank.v1beta1.MsgSend', MsgSend as GeneratedType],
-    ['/cosmos.bank.v1beta1.MsgMultiSend', MsgMultiSend as GeneratedType],
-    ['/cosmos.distribution.v1beta1.MsgFundCommunityPool', MsgFundCommunityPool as GeneratedType],
-    ['/cosmos.distribution.v1beta1.MsgSetWithdrawAddress', MsgSetWithdrawAddress as GeneratedType],
-    ['/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward', MsgWithdrawDelegatorReward as GeneratedType],
-    ['/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission', MsgWithdrawValidatorCommission as GeneratedType],
-    ['/cosmos.staking.v1beta1.MsgBeginRedelegate', MsgBeginRedelegate as GeneratedType],
-    ['/cosmos.staking.v1beta1.MsgCreateValidator', MsgCreateValidator as GeneratedType],
-    ['/cosmos.staking.v1beta1.MsgDelegate', MsgDelegate as GeneratedType],
-    ['/cosmos.staking.v1beta1.MsgEditValidator', MsgEditValidator as GeneratedType],
-    ['/cosmos.staking.v1beta1.MsgUndelegate', MsgUndelegate as GeneratedType],
+    ['/cosmos.auth.v1beta1.BaseAccount', BaseAccount],
+    ['/cosmos.auth.v1beta1.ModuleAccount', ModuleAccount],
+    ['/cosmos.auth.v1beta1.Params', AuthParams],
+    ['/cosmos.authz.v1beta1.MsgGrant', MsgGrant],
+    ['/cosmos.authz.v1beta1.MsgExec', MsgExec],
+    ['/cosmos.authz.v1beta1.MsgRevoke', MsgRevoke],
+    ['/cosmos.bank.v1beta1.MsgSend', MsgSend],
+    ['/cosmos.bank.v1beta1.MsgMultiSend', MsgMultiSend],
+    ['/cosmos.base.v1beta1.Coin', Coin],
+    ['/cosmos.base.v1beta1.DecCoin', DecCoin],
+    ['/cosmos.base.v1beta1.IntProto', IntProto],
+    ['/cosmos.base.v1beta1.DecProto', DecProto],
+    ['/cosmos.crypto.ed25519.PubKey', PubKey],
+    ['/cosmos.crypto.secp256k1.PubKey', PubKey],
+    ['/cosmos.distribution.v1beta1.MsgFundCommunityPool', MsgFundCommunityPool],
+    ['/cosmos.distribution.v1beta1.MsgSetWithdrawAddress', MsgSetWithdrawAddress],
+    ['/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward', MsgWithdrawDelegatorReward],
+    ['/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission', MsgWithdrawValidatorCommission],
+    ['/cosmos.distribution.v1beta1.CommunityPoolSpendProposal', CommunityPoolSpendProposal],
+    ['/cosmos.distribution.v1beta1.CommunityPoolSpendProposalWithDeposit', CommunityPoolSpendProposalWithDeposit],
+    ['/cosmos.feegrant.v1beta1.MsgGrantAllowance', MsgGrantAllowance],
+    ['/cosmos.feegrant.v1beta1.MsgRevokeAllowance', MsgRevokeAllowance],
     ['/cosmos.gov.v1beta1.MsgDeposit', MsgDeposit],
     ['/cosmos.gov.v1beta1.MsgSubmitProposal', MsgSubmitProposal],
     ['/cosmos.gov.v1beta1.MsgVote', MsgVote],
+    ['/cosmos.gov.v1beta1.Proposal', Proposal],
+    ['/cosmos.gov.v1beta1.TextProposal', TextProposal],
+    ['/cosmos.params.v1beta1.ParameterChangeProposal', ParameterChangeProposal],
+    ['/cosmos.slashing.v1beta1.MsgUnjail', MsgUnjail],
+    ['/cosmos.staking.v1beta1.MsgBeginRedelegate', MsgBeginRedelegate],
+    ['/cosmos.staking.v1beta1.MsgCreateValidator', MsgCreateValidator],
+    ['/cosmos.staking.v1beta1.MsgDelegate', MsgDelegate],
+    ['/cosmos.staking.v1beta1.MsgEditValidator', MsgEditValidator],
+    ['/cosmos.staking.v1beta1.MsgUndelegate', MsgUndelegate],
+    ['/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal', SoftwareUpgradeProposal],
+    ['/cosmos.upgrade.v1beta1.CancelSoftwareUpgradeProposal', CancelSoftwareUpgradeProposal],
     ['/ibc.core.channel.v1.MsgChannelOpenInit', MsgChannelOpenInit],
     ['/ibc.core.channel.v1.MsgChannelOpenTry', MsgChannelOpenTry],
     ['/ibc.core.channel.v1.MsgChannelOpenAck', MsgChannelOpenAck],
@@ -61,9 +91,9 @@ const registryTypes: Iterable<[string, GeneratedType]> = [
     ['/ibc.core.connection.v1.MsgConnectionOpenAck', MsgConnectionOpenAck],
     ['/ibc.core.connection.v1.MsgConnectionOpenConfirm', MsgConnectionOpenConfirm],
     ['/ibc.applications.transfer.v1.MsgTransfer', MsgTransfer],
-    ['/lum.network.beam.MsgOpenBeam', MsgOpenBeam as GeneratedType],
-    ['/lum.network.beam.MsgUpdateBeam', MsgUpdateBeam as GeneratedType],
-    ['/lum.network.beam.MsgClaimBeam', MsgClaimBeam as GeneratedType],
+    ['/lum.network.beam.MsgOpenBeam', MsgOpenBeam],
+    ['/lum.network.beam.MsgUpdateBeam', MsgUpdateBeam],
+    ['/lum.network.beam.MsgClaimBeam', MsgClaimBeam],
 ];
 
 class ExtendedRegistry extends Registry {
