87a6bca42f7356a3bcd977e7b657a37d21f885c1
Thibault Jaillard <thibault.jaillard@gmail.com>
2022-09-22 11:17:26 +0200

    Add createdAt into MsgDepositDfract

--- src/messages/dfract/MsgDepositDfract.ts
+++ src/messages/dfract/MsgDepositDfract.ts
@@ -4,12 +4,13 @@ import { Coin } from '../../types';
 
 export const MsgDepositDfractUrl = '/lum.network.dfract.MsgDepositDfract';
 
-export const BuildMsgDepositDfract = (depositorAddress: string, amount: Coin): Message => {
+export const BuildMsgDepositDfract = (depositorAddress: string, amount: Coin, createdAt: Date): Message => {
     return {
         typeUrl: MsgDepositDfractUrl,
         value: {
             depositorAddress,
             amount,
+            createdAt,
         } as MsgDepositDfract,
     };
 };
