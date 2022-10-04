c2ea37f06691181c5b23604698ba8e486ef055f7
Thibault Jaillard <thibault.jaillard@gmail.com>
2022-10-04 11:50:13 +0200

    [LUM-425] Register WithdrawAndMintProposal type url (#49)
    * Register WithdrawAndMintProposal type url
    
    * Bump version to 0.7.11

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -38,6 +38,7 @@ import { MsgTransfer } from '../codec/ibc/applications/transfer/v1/tx';
 
 import { MsgClaimBeam, MsgOpenBeam, MsgUpdateBeam } from '../codec/beam/tx';
 import { MsgDeposit as MsgDepositDfract } from '../codec/dfract/tx';
+import { WithdrawAndMintProposal } from '../codec/dfract/proposal';
 
 const registryTypes: Iterable<[string, GeneratedType]> = [
     ['/cosmos.auth.v1beta1.BaseAccount', BaseAccount],
@@ -104,6 +105,7 @@ const registryTypes: Iterable<[string, GeneratedType]> = [
     ['/lum.network.beam.MsgUpdateBeam', MsgUpdateBeam],
     ['/lum.network.beam.MsgClaimBeam', MsgClaimBeam],
     ['/lum.network.dfract.MsgDeposit', MsgDepositDfract],
+    ['/lum.network.dfract.WithdrawAndMintProposal', WithdrawAndMintProposal],
 ];
 
 class ExtendedRegistry extends Registry {
