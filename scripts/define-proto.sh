4263ff56524e87f1942c6d19bc490a65b5fe18a6
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-01-24 15:47:31 +0100

    Adapt gov module to use v1 path

--- scripts/define-proto.sh
+++ scripts/define-proto.sh
@@ -52,6 +52,9 @@ protoc \
     "$COSMOS_PROTO_DIR/cosmos/gov/v1/gov.proto" \
     "$COSMOS_PROTO_DIR/cosmos/gov/v1/query.proto" \
     "$COSMOS_PROTO_DIR/cosmos/gov/v1/tx.proto" \
+    "$COSMOS_PROTO_DIR/cosmos/gov/v1beta1/gov.proto" \
+    "$COSMOS_PROTO_DIR/cosmos/gov/v1beta1/query.proto" \
+    "$COSMOS_PROTO_DIR/cosmos/gov/v1beta1/tx.proto" \
     "$COSMOS_PROTO_DIR/cosmos/mint/v1beta1/mint.proto" \
     "$COSMOS_PROTO_DIR/cosmos/mint/v1beta1/query.proto" \
     "$COSMOS_PROTO_DIR/cosmos/params/v1beta1/params.proto" \
